1cdd954335882654db82891d5e36bb6481486d74
Jacqueline Cameron <jskye429@mail.com>
2020-02-24 21:48:31 -0600

    added more folders and started mongo setup

--- controllers/framescontroller.js
+++ controllers/framescontroller.js
@@ -3,4 +3,17 @@ const db = require("../models");
 // Defining methods for the booksController
 module.exports = {
   findAll: function(req, res) {
-    db.Frames 
+    db.Frames   
+    .find(req.query)
+    .sort({ Series: -1 })
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
+  },
+findBySKU: function(req, res) {
+        db.Frames
+          .findBySKU(req.params.id)
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+      }
+
+    };
