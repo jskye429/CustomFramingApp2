7b0462f09385b0ebec0a899186564bb37bb2fc24
Jacqueline Cameron <jskye429@mail.com>
2020-02-29 12:07:09 -0600

    fixed some path and server issues

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const path = require("path");
-const router = require("express").Router;
-const apiRoutes = require("api");
+const router = require("express").Router();
+const frameRoutes = require("./api/frames");
 
-router.use("/api", apiRoutes);
+router.use("/api/frames", frameRoutes);
 
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,22 +3,35 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const bodyParser = require("body-parser");
+// const cors = require("cors");
+const PORT = process.env.PORT || 8081;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use(cors);
+app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+app.use("/", routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/studioFrames");
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
 
+// app.listen(PORT, function() {
+//   console.log("Server is running on Port: " + PORT);
+// });
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
