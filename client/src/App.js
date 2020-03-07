28dd99fe71de7f2cc55cfd47e3dc1de9fc18a52c
Ellen <evwhite2@gmail.com>
2020-03-07 11:10:40 -0600

    merging signup conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,12 @@ import "./App.css";
 import Nav from "./components/Nav/Nav";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./components/catalogue/Home";
-<<<<<<< HEAD
-import Login from "./components/login/navbar";
-import Cart from "./components/cart/Cart.js";
-import QuoteSpec from "./components/quote/QuoteSpec";
 
-import NavBar from "./components/login/navbar";
-=======
+import NavBar from "./components/Login/navbar";
 import Login from "./components/Login/Login";
 import Cart from "./components/Cart/Cart.js";
 import QuoteSpec from "./components/Quote/QuoteSpec";
 import newUser from "./components/Login/NewUser";
-
->>>>>>> master
-
 
 
 function App() {
