86062ef2c3661525c9e7b832516b64b681a39626
Ellen <evwhite2@gmail.com>
2020-03-11 17:18:02 -0500

    login/Login

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,10 @@ import Nav from "./components/Nav/Nav";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./components/catalogue/Home";
 import Cart from "./components/Cart/Cart";
-import Login from "./components/Login/Login";
+import Login from "./components/Login/Login"
 import NavBar from "./components/Login/authenticate";
 import QuoteSpec from "./components/quote/QuoteSpec";
-import newUser from "./components/login/NewUser";
+import newUser from "./components/Login/NewUser";
 import Art from "./components/Art/Art";
 
 
@@ -15,7 +15,7 @@ function App() {
   return (
     <BrowserRouter>
       <div>
-        <Nav />
+        <Nav /><NavBar/>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/cart" component={Cart} />
