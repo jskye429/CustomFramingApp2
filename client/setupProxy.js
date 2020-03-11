36749bb261233d9f80113a74fb729695638a58ff
Ellen <evwhite2@gmail.com>
2020-03-10 22:58:36 -0500

    proxy

--- client/setupProxy.js
+++ client/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy(["mongodb://whitestudioframes2020:studio2020@ds033797.mlab.com:33797/heroku_5875wdc2", "/api/cart", "/api/frames", "/api/cart"], { target: "http://localhost:8081" })
+    proxy(["/", "/api/cart", "/api/frames", "/api/cart"], { target: "http://localhost:8081" })
   );
 };
 
