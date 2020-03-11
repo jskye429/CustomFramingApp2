b6e3a587c324e752573a7d7968a03b2d1b6fde2a
Ellen <evwhite2@gmail.com>
2020-03-10 23:46:28 -0500

    removing build from client/gitignore, adding proxy 8081 back

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 8081;
 
-const HOST = process.env.HOST || "custom-studio-frames-2020.herokuapp.com"
+const HOST = process.env.HOST || "custom-studio-frames-2020.herokuapp.com";
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
